1a0d96ae199525b189d55bfa14c05f72bd0c36de
小泉勝志郎 <jigoku.hen@gmail.com>
2020-04-04 14:48:19 +0900

    - generate時のエラーを修正

--- components/DataView.vue
+++ components/DataView.vue
@@ -257,7 +257,7 @@ export default Vue.extend({
         'https://twitter.com/intent/tweet?text=' +
         this.title +
         ' / ' +
-        '宮城県非公式'+
+        '宮城県非公式' +
         this.$t('新型コロナウイルス感染症') +
         this.$t('対策サイト') +
         '&url=' +
