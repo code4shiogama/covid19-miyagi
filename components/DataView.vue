5d07099821af6fcbda4ec7111ef35d0cf84af5f7
小泉勝志郎 <jigoku.hen@gmail.com>
2020-04-06 15:55:58 +0900

    - 1時間単位でOGP画像更新

--- components/DataView.vue
+++ components/DataView.vue
@@ -250,6 +250,13 @@ export default Vue.extend({
       if (host) {
         permalink = location.protocol + '//' + location.host + permalink
       }
+        var dt = new Date();
+        var y = dt.getFullYear();
+        var m = ("00" + (dt.getMonth()+1)).slice(-2);
+        var d = ("00" + dt.getDate()).slice(-2);
+        var h = ("00" + dt.getHours()).slice(-2);
+        var result = y + m + d + h;
+        permalink += "&t=" + result;
       return permalink
     },
     twitter() {
